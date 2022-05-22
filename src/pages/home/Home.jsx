2aeb8eb5e3e53167bce39c3949b2655062d62d67
Ignas Kiaulakys <ignas.kiaulakys@gmail.com>
2022-05-22 20:24:51 +0300

    Implemented routes

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Home = () => {
+  return <div>Home ss</div>;
+};
+
+export default Home;
---
+++ src/pages/hotel/Hotel.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Hotel = () => {
+  return <div>Hello</div>;
+};
+export default Hotel;
